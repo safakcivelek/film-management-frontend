8128a85bef32a03ac37b6ce5ea3df1d9a353a7a9
safakcivelek <safakcivelek3@gmail.com>
2024-09-04 15:03:14 +0300

    Refactor: Optimize page-specific context providers and prevent redundant API requests (App.js)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/layout/navbar/Navbar';
@@ -13,16 +13,15 @@ import ContactPage from './pages/Contact/ContactPage';
 import LoginPage from './pages/Login/LoginPage';
 import RegisterPage from './pages/Register/RegisterPage';
 import FilmDetailPage from './pages/FilmDetail/FilmDetailPage';
-import { FilmProvider } from './contextApi/FilmContext';
 import { FilmFilterProvider } from './contextApi/FilmFilterContext';
 import { AuthProvider } from './contextApi/AuthContext';
+import { FilmDetailProvider } from './contextApi/FilmDetailContext';
+import { HomePageFilmProvider } from './contextApi/HomePageFilmContext';
 
-function App() { // AuthProvider nerde olacak?
+function App() {
   return (
     <Router>
       <AuthProvider>
-      <FilmProvider>
-        <FilmFilterProvider>
         <div id="root">
           <Navbar />
 
@@ -31,24 +30,54 @@ function App() { // AuthProvider nerde olacak?
             autoClose={5000}
             hideProgressBar={false}
             newestOnTop={false}
-            closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
 
           <main style={{ flex: '1' }}>
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/films" element={<FilmListPage />} />
-              <Route path="/film/:id" element={<FilmDetailPage />} />
+              {/* FilmProvider sadece HomePage için kullanılıyor */}
+              <Route
+                path="/"
+                element={
+                  <HomePageFilmProvider>
+                    <HomePage />
+                  </HomePageFilmProvider>
+                }
+              />
+
+              {/* FilmListPage sadece FilmFilterProvider ile çalışıyor */}
+              <Route
+                path="/films"
+                element={
+                  <FilmFilterProvider>
+                    <FilmListPage />
+                  </FilmFilterProvider>
+                }
+              />
+
+              {/* FilmDetailProvider sadece film detay sayfasını sarmalıyor */}
+              <Route
+                path="/film/:id"
+                element={
+                  <FilmDetailProvider>
+                    <FilmDetailPage />
+                  </FilmDetailProvider>
+                }
+              />
+
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route path="/about" element={< AboutPage />} />
+              <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<ContactPage />} />
             </Routes>
           </main>
 
-          <Footer />         
+          <Footer />
         </div>
-        </FilmFilterProvider>
-      </FilmProvider>
       </AuthProvider>
     </Router>
   );
