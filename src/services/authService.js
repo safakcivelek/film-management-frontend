a7c155f8c656fa97cbe985cd6a93b958b2fbc296
safakcivelek <safakcivelek3@gmail.com>
2024-09-16 19:36:14 +0300

    Added register method to AuthService for user registration functionality.

--- src/services/authService.js
+++ src/services/authService.js
@@ -13,11 +13,13 @@ class AuthService extends BaseService {
             .catch(this.handleError);
     }
 
-    testAuthorize() {
-        return apiClient.get(`${this.apiUrl}/test-authorize`)
+    // Register metodu
+    register(firstName, lastName, email, password, confirmPassword) {
+        const data = { firstName, lastName, email, password, confirmPassword };
+        return apiClient.post(`${this.apiUrl}/register`, data)
             .then(response => response.data)
             .catch(this.handleError);
-    }
+    }  
 }
 
 export default new AuthService();
